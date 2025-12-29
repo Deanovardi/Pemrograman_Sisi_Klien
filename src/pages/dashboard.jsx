ded3c9f000e543a088dbd664df37a9a79cd1d803
deanovardi <deanovaardi6@gmail.com>
2025-12-29 23:17:49 +0700

    Component with UI Library

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -7,7 +7,7 @@ import CardUpcomingBill from '../components/Fragments/CardUpcomingBill';
 import CardRecentTransaction from '../components/Fragments/CardRecentTransaction';
 import CardStatistic from '../components/Fragments/CardStatistic';
 import CardExpenseBreakdown from '../components/Fragments/CardExpenseBreakdown';
-import { bills, expensesBreakdowns, transactions } from '../data';
+import { bills, expensesBreakdowns, goals, transactions, balances, expensesStatistics } from '../data';
 
 function dashboard() {
     console.log(transactions);
@@ -17,10 +17,10 @@ function dashboard() {
       			<MainLayout>
         <div className="grid sm:grid-cols-12  gap-6 ">
           <div className="sm:col-span-4">
-            <CardBalance />
+            <CardBalance data={balances} />
           </div>
           <div className="sm:col-span-4">
-            <CardGoal />
+            <CardGoal data={goals} />
           </div>
           <div className="sm:col-span-4">
             <CardUpcomingBill data={bills} />
@@ -29,7 +29,7 @@ function dashboard() {
             <CardRecentTransaction data={transactions} />
           </div>
           <div className="sm:col-span-8">
-            <CardStatistic />
+            <CardStatistic data={expensesStatistics} />
           </div>
           <div className="sm:col-span-8">
             <CardExpenseBreakdown data={expensesBreakdowns} />
