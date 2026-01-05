683001cf7e9eea158f49f0ed5869fbf14ac46474
deanovardi <deanovaardi6@gmail.com>
2026-01-06 00:23:46 +0700

    Frontend-Backend Integration

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import MainLayout from '../components/Layouts/MainLayout';
-import Card from '../components/Elements/Card';
 import CardBalance from '../components/Fragments/CardBalance'; 
 import CardGoal from '../components/Fragments/CardGoal';
 import CardUpcomingBill from '../components/Fragments/CardUpcomingBill';
 import CardRecentTransaction from '../components/Fragments/CardRecentTransaction';
 import CardStatistic from '../components/Fragments/CardStatistic';
 import CardExpenseBreakdown from '../components/Fragments/CardExpenseBreakdown';
-import { bills, expensesBreakdowns, goals, transactions, balances, expensesStatistics } from '../data';
+import { 
+  bills, 
+  expensesBreakdowns, 
+  goals, 
+  transactions, 
+  balances, 
+  expensesStatistics, 
+} from '../data';
+import { goalService } from '../services/dataService';
+import { AuthContext } from '../context/authContext';
 
-function dashboard() {
-    console.log(transactions);
+function dashboard() {	
+  const [goals, setGoals] = useState({});
+  const { logout } = useContext(AuthContext);
+
+  const fetchGoals = async () => {
+    try {
+      const data = await goalService();
+      setGoals(data);
+    } catch (err) {
+      console.error("Gagal mengambil data goals:", err);
+      if (err.status === 401) {
+        logout();
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchGoals();
+  }, []);
+  
+  console.log(goals);
 
   return (
     <>
